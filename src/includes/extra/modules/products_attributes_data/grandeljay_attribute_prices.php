3ba876b5c5b0d399e81729a6e7e59125535c0e5b
grandeljay <github@grandels.email>
2024-02-23 16:18:16 +0100

    fix: undefined variable `$customer_status`

--- src/includes/extra/modules/products_attributes_data/grandeljay_attribute_prices.php
+++ src/includes/extra/modules/products_attributes_data/grandeljay_attribute_prices.php
@@ -29,8 +29,12 @@ if (!empty($products_options['attributes_model'])) {
         return;
     }
 
-    $xtc_price      = new \xtcPrice($currencies_array[0]['id'], $customer_status['customers_status']);
-    $products_price = $xtc_price->xtcGetPrice($products_data['products_id'], false);
+    $currency_id         = $currencies_array[0]['id'];
+    $customers_status_id = $customer_status['customers_status'] ?? \DEFAULT_CUSTOMERS_STATUS_ID_GUEST;
+    $products_id         = $products_data['products_id'];
+
+    $xtc_price      = new \xtcPrice($currency_id, $customers_status_id);
+    $products_price = $xtc_price->xtcGetPrice($products_id, false);
 
     $products_options_data[$row]['DATA'][$col]['PRICE']       = $xtc_price->xtcFormat($products_price, true);
     $products_options_data[$row]['DATA'][$col]['FULL_PRICE']  = $xtc_price->xtcFormat($products_price, true);
